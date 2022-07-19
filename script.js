c4dc633e22dde09b9fd71adc3fdaa49141c5aa18
L3m0n Cao <96365455+L3m0n-Cao@users.noreply.github.com>
2022-07-19 19:52:17 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let number = prompt("What Multiplication do you want?", "3"); //Prompt what multiplication, default is 3.
 let multiples = prompt("Thanks. How many Multiples do you want?", "20"); //prompt how many times, default is 20, as picked from word doc.
 
-document.write("Multiplication: " + number); //show what multiplication you picked
-document.write("How Many Multiples: " + multiples); //how many multiples you pick
+console.log("Multiplication: " + number); //show what multiplication you picked
+console.log("How Many Multiples: " + multiples); //how many multiples you pick
 
 var fix = (multiples*multiples/multiples+1); //does some trickery to change varible to a proper number, and add 1. 
 
-document.write("Results:");
 for (var i = 1; i < fix; i++) { //loop
   var logging = (number*i); //multiplying 
   console.log(logging); //console logging
